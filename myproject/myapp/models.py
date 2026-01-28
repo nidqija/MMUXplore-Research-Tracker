02cafbfcec2d1b5c80b9915c4fcd7b5723e398e4
Raziq Din <ajiqdin646@gmail.com>
2026-01-29 05:54:43 +0800

    added user profile page (static)

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 # =================================== User Model ======================================== 
 
 class User(models.Model):
+     user_id = models.AutoField(primary_key=True)
      fullname = models.CharField(max_length=150 , default='unknown')
      university_id = models.CharField(max_length=50, unique=True , default='0000')
      email = models.EmailField(unique=True , default='unknown@example.com')
@@ -75,4 +76,25 @@ class Announcements(models.Model):
      @property
      def is_new(self):
           return timezone.now() - self.date_posted <= timedelta(hours=24)
+     
+
+class Violations(models.Model):
+     violation_id = models.AutoField(primary_key=True)
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     violation_type = models.TextField()
+     date_reported = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+          return f"Violation {self.violation_id} by {self.user.fullname}"
+     
+
+class Violation_User(models.Model):
+     violation_user_id = models.AutoField(primary_key=True)
+     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+     violation_id = models.ForeignKey(Violations, on_delete=models.CASCADE)
+     date_recorded = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+          return f"User {self.user_id.fullname} - Violation {self.violation_id.violation_id}"
+     
      
