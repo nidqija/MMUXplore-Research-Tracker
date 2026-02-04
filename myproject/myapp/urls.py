f15fb47f9e4287c9e0c63dcb7efef88953c4cda6
Raziq Din <ajiqdin646@gmail.com>
2026-02-04 18:08:43 +0800

    added view terms and conditions page dynamically according to roles

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('avatar_register/', views.user_avatar_register, name='avatar_register'),
     path('update_profile/', views.update_profile, name='update_profile'),
     path('view_announcements/<int:announcement_id>/', views.view_announcement_page, name='view_announcements'),
+    path('term_condition_page/', views.term_condition_page, name='term_condition_page' ),
     path('logout/', views.user_logout, name='logout'),
     # End General URLs
 
@@ -27,7 +28,6 @@ urlpatterns = [
 
     # Admin URLs
     path('adminguy/admin_page/', views.admin_page, name='admin_homepage' ),
-    path('adminguy/term_condition_page/', views.term_condition_page, name='term_condition_page' ),
     path('adminguy/delete_term/<int:term_id>/', views.delete_term_condition, name='delete_term_condition' ),
     path('adminguy/update_term/<int:term_id>/', views.update_term_condition, name='update_term_condition' ),
     path('adminguy/announcement_page/', views.announcement_page, name='announcement_page' ),
